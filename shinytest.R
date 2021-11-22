f87f65110dcc1b4d5c5b8c9efdc0ae992104b6c0
nightmaresinclr <85890384+nightmaresinclr@users.noreply.github.com>
2021-11-21 22:42:07 -0600

    Add files via upload
    Updated layout

--- shinytest.R
+++ shinytest.R
@@ -1,49 +1,51 @@
 library(shiny)
 ui <- fluidPage(
   tabsetPanel(type = "tabs",
-              tabPanel("Home", plotOutput("plot")),
-              tabPanel("Sentiment Analysis", verbatimTextOutput("summary")),
-              tabPanel("Design", tableOutput("table")),
-              tabPanel("Acknowledgements", tableOutput("table")),
-              tabPanel("Reflection", tableOutput("table"))
-  )
+              tabPanel("Sentiment Analysis", 
+                       textInput("typedInput", "", value = "", width = "400px", placeholder = "Enter text to be analyzed here..."),
+                       actionButton("analyzeButton", "Analyze"),
+                       actionButton("fileInput", "Import a file..."),
+                       sidebarLayout(
+                         sidebarPanel(
+                           "Confidence Level: ", textOutput("confidence", inline = FALSE)
+                         ),
+                         mainPanel(
+                           "Output Emoji: ", textOutput("emoji", inline = FALSE)
+                         )
+                       ),
+                       headerPanel("Visualizations:"),
+                       "Awaiting input..."
+                       ),
+              tabPanel("Design", "Design will go here! With charts and images."),
+              tabPanel("Acknowledgements", "Acknowledgements will go here!"),
+              tabPanel("Reflection", "Reflection will go here!")
+  ),
+  
 )
 
 server <- function(input, output, session) {
   # the modal dialog where the user can enter the query details.
-  query_modal <- modalDialog(
-    title = "Welcome to our project!",
-    easyClose = F,
-    footer = tagList(
-      actionButton("run", "Close")
-    )
-  )
+  #query_modal <- modalDialog(
+  #  title = "Welcome to our project!",
+  #  easyClose = F,
+  #  footer = tagList(
+  #    actionButton("run", "Close")
+  #  )
+  #)
   
   
   # Show the model on start up ...
-  showModal(query_modal)
+  #showModal(query_modal)
   
   
   # ... or when user wants to change query
-  observeEvent(input$change,
-               {
-                 showModal(query_modal)
-               })
+  #observeEvent(input$change,
+  #             {
+  #               showModal(query_modal)
+  #             })
   
   # reactiveVal to store the dataset
-  my_dataset <- reactiveVal()
-  
-  observeEvent(input$run, {
-    removeModal()
-    
-    # Your query here
-    my_data <- mtcars %>% filter(cyl %in% input$input_query)
-    my_dataset(my_data)
-    
-  })
-  
-  # render the output
-  output$my_table <- renderDataTable(my_dataset())
+
   
 }
 
